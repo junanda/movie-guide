ea898efece39bdf83a376dda90bdbd7b4475aebc
junanda.patihullah <junanda.patihullah@spesolution.com>
2023-08-18 09:32:17 +0700

    update page and create components info

---
+++ src/lib/components/Info.svelte
@@ -0,0 +1,32 @@
+<script>
+    import ratingStart from '$lib/images/star-icon.svg';
+</script>
+
+<div class="info">
+    <img src="" alt="" class="poster"/>
+    <div>
+        <h2>Title</h2>
+        <div class="rating">
+            <img src={ratingStart} alt="star" />
+            <h4>rating</h4>
+        </div>
+        <div class="details">
+            <span>Rated</span>
+            <span>Year</span>
+            <span>Runtime</span>
+        </div>
+        <div class="genre">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+</div>
+<h3>Plot:</h3>
+<p>detail plot</p>
+<h3>Cast</h3>
+<p>Actors</p>
+
+<style>
+
+</style>
